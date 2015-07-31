e4e7c1466ecd1d507139d1862e93d30cd2e3ebdc
Arvind Naidu <arnaidu@paypal.com>
2015-07-31 14:10:59 -0700

    auth

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static('public'));
 
+app.get("/auth", function (req, res) {
+  res.status(200).send(req.params);
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
